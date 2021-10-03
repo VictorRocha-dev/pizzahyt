ecdd4eb6d1edbafae7f1ccf2b3fe55d577adc4ea
Victor Souza Rocha <75147285+VictorRocha-dev@users.noreply.github.com>
2021-10-02 22:23:23 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,9 @@
+function onClickMenu(){
+    document.getElementById("menu").classList.toggle("change")
+    document.getElementById("nav").classList.toggle("change")
+    document.getElementById("menu-bg").classList.toggle("change")
+}
+
+function click(){
+    window.location = "html\cardapio\index.html";
+}
